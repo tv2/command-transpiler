a38993ba29315744f1f7f1172c88df1f25d68811
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-08-10 12:24:50 +0200

    Added better types for transpiler interface

--- src/transpiler.ts
+++ src/transpiler.ts
@@ -1,6 +1,10 @@
 import { Rules } from './rules'
 import { ModifierDomain } from './modifier'
 
+interface IVariables {
+ [key: string]: any
+}
+
 export class Transpiler {
   protected rules: Rules
   constructor(rules: Rules = [], modifiers?: ModifierDomain) {
@@ -8,11 +12,11 @@ export class Transpiler {
     if (modifiers) this.rules.forEach(rule => rule.injectWith(modifiers))
   }
 
-  transpile(input: string) {
+  transpile(input: string, variables: IVariables = {}): { line: number, result: string } {
     for (const [line,rule] of this.rules.entries()) {
       const result = rule.match(input)
       if (result !== null) {
-        return { line, result: rule.fill(result) }
+        return { line, result: rule.fill({ ...variables, ...result }) }
       }
     }
     throw new Error(`No transpilation rule matched the input: ${input}`)
