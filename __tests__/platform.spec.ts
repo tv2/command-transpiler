19b5d858ee07cd425c414d1401625618bdfc138d
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-09-02 22:13:09 +0200

    feat: triggering workflow

--- __tests__/platform.spec.ts
+++ __tests__/platform.spec.ts
@@ -2,10 +2,8 @@ type Platform = 'aix' | 'darwin' | 'freebsd' | 'linux' | 'openbsd' | 'sunos' | '
 
 export function runPlatform(platform: Platform, fun: Function) {
   const hostPlatform = process.platform
-  Object.defineProperty(process, 'platform', { value: platform });
+  Object.defineProperty(process, 'platform', { value: platform })
   const result = fun()
-  Object.defineProperty(process, 'platform', { value: hostPlatform });
+  Object.defineProperty(process, 'platform', { value: hostPlatform })
   return result
 }
-
-
